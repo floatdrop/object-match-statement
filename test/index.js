c82ae51e05e42992376f25ce04d8aa315a4249af
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-27 22:43:42 +0600

    Added test for escaped quote

--- test/index.js
+++ test/index.js
@@ -13,39 +13,38 @@ describe('escape', function () {
     });
 
     it('should escape invalid identifiers with double quotes', function() {
-        Matcher.escape('"invalid').should.eql('["\\\"invalid"]');
+        Matcher.escape('"invalid').should.eql('["\\"invalid"]');
+    });
+
+    it('should escape invalid identifiers with escaped double quotes', function() {
+        Matcher.escape('button\\"1').should.eql('["button\\\\"1"]');
     });
 });
 
 describe('matcher', function () {
     it('should return true on patter-match', function() {
-        var matcher = Matcher.build('object', {a: 1});
-        var func = new Function('object', 'return ' + matcher);
-        func({a: 1, c: 1}).should.be.ok;
+        var matcher = Matcher.compile({a: 1});
+        matcher({a: 1, c: 1}).should.be.ok;
     });
 
     it('should return false on pattern-mismatch', function() {
-        var matcher = Matcher.build('object', {a: 2});
-        var func = new Function('object', 'return ' + matcher);
-        func({a: 1, c: 1}).should.not.be.ok;
+        var matcher = Matcher.compile({a: 2});
+        matcher({a: 1, c: 1}).should.not.be.ok;
     });
 
     it('should return true on pattern-match with complex object', function() {
-        var matcher = Matcher.build('object', {a: {b: 1}});
-        var func = new Function('object', 'return ' + matcher);
-        func({a: {b: 1}, c: 1}).should.be.ok;
+        var matcher = Matcher.compile({a: {b: 1}});
+        matcher({a: {b: 1}, c: 1}).should.be.ok;
     });
 
     it('should return false on pattern-mismatch with complex object', function() {
-        var matcher = Matcher.build('object', {a: {b: 2}});
-        var func = new Function('object', 'return ' + matcher);
-        func({a: {b: 1}, c: 1}).should.not.be.ok;
+        var matcher = Matcher.compile({a: {b: 2}});
+        matcher({a: {b: 1}, c: 1}).should.not.be.ok;
     });
 
     it('should escape compared value of object', function() {
-        var matcher = Matcher.build('object', { a: '"wow'} );
-        var func = new Function('object', 'return ' + matcher);
-        func({a: '"wow'}).should.be.ok;
+        var matcher = Matcher.compile({ a: '"wow'} );
+        matcher({a: '"wow'}).should.be.ok;
     });
 });
 
